8e8aa984e408fc37be2b39135ca4833cf60bd73f
Amanda Fillip <afillip@windstream.net>
2016-07-18 19:52:40 -0500

    readme added and refractored code

---
+++ README.md
@@ -0,0 +1,7 @@
+Future Features:
+	- only append unique titles...stop repitions
+	- search not found
+	- more searchability
+	-streamline ui search
+	- css
+	-react
--- client/app.js
+++ client/app.js
@@ -9,9 +9,7 @@
 			}).complete(function(data){
 				var entries = data.responseJSON;
 				console.log("scrape3: ", entries)
-	 			for(var i=0; i<entries.links.length; i++){
-	 				$('.linkTable').append(`<tr><td><img src='${entries.images[i]}' width=175 height=175></img></td><td><a href='${entries.links[i]}'>${entries.titles[i]}</a></td></tr>`)
-	 			}
+				toList(entries)
 				$.ajax({
 					url: 'scrape2',
 					type: 'GET',
@@ -20,9 +18,7 @@
 				}).complete(function(data){
 					var entries = data.responseJSON;
 					console.log("scrape2: ", entries)
-					for(var i=0; i<entries.links.length; i++){
-						$('.linkTable').append(`<tr><td><img src='${entries.images[i]}' width=175 height=175></img></td><td><a href='${entries.links[i]}'>${entries.titles[i]}</a></td></tr>`)
-					}
+					toList(entries)
 					$.ajax({
 						url: 'scrape',
 						type: 'GET',
@@ -31,10 +27,7 @@
 						}).complete(function(data){
 							console.log("client-scrape: ", data.responseJSON)
 							var entries = data.responseJSON
-							for(var i=0; i<entries.links.length; i++){
-								$('.linkTable').append(`<tr><td><img src='${entries.images[i]}' width=175 height=175></img></td><td><a href='${entries.links[i]}'>${entries.titles[i]}</a></td></tr>`)
-							}
-
+							toList(entries)
 					})
 				})
 
@@ -45,16 +38,17 @@
 
 
 //-------------------------------Get Music by Filter---------------//
+var holder = {
+				links: [],
+				images: [],
+				titles: [],
+				blurbs: [],
+				genres: []
+			}; 
+var genre;
  $('.searchMusic').submit(function(e){
  	e.preventDefault()
- 	var genre = $(".genre").val()
- 	var holder = {
- 					links: [],
- 					images: [],
- 					titles: [],
- 					blurbs: [],
- 					genres: []
- 				};
+ 	genre = $(".genre").val()
 	$.ajax({
 			url: 'scrape3',
 			type: 'GET',
@@ -62,12 +56,7 @@
 		 	dataType: 'json'
 			}).complete(function(data){
 				 var entries = data.responseJSON;
-				 for(var i=0;i<entries['links'].length;i++){
-					 holder['links'].push(entries.links[i]);
-					 holder['images'].push(entries.images[i]);
-					 holder['titles'].push(entries.titles[i]);
-					 holder['blurbs'].push(entries.blurbs[i]);			 	
-				 }
+				 toHolder(entries)
 				$.ajax({
 					url: 'scrape2',
 					type: 'GET',
@@ -75,12 +64,7 @@
 					dataType: 'json'
 				}).complete(function(data){
 					var entries = data.responseJSON;
-					for(var i=0; i<entries['links'].length;i++){
-						holder['links'].push(entries.links[i]);
-						holder['images'].push(entries.images[i]);
-						holder['titles'].push(entries.titles[i]);
-						holder['blurbs'].push(entries.blurbs[i]);
-					}	
+					toHolder(entries)	
 					$.ajax({
 						url: 'scrape',
 						type: 'GET',
@@ -88,39 +72,13 @@
 					 	dataType: 'json'
 						}).complete(function(data){
 							var entries = data.responseJSON
-							for(var j=0; j<entries['links'].length; j++){
-								holder['links'].push(entries.links[j]);
-								holder['images'].push(entries.images[j]);
-								holder['titles'].push(entries.titles[j]);
-								holder['blurbs'].push(entries.blurbs[j]);
-							}
+							toHolder(entries)
 							for(var k=2;k<entries.genres.length;k+=3){
 									holder['genres'].push(entries.genres[k])	
 							}
-							for(var i=0; i < holder.blurbs.length; i++){
-								if(holder.blurbs[i]){
-									var link = holder.blurbs[i];
-									var substrings = [genre];
-									var length = substrings.length;
-									 while(length--){
-										if(link.indexOf(substrings[length])!=-1){
-											$('.linkTable').append(`<tr><td><img src='${holder.images[i]}' width=175 height=175></img></td><td><a href='${holder.links[i]}'>${holder.titles[i]}</a></td></tr>`)							 	
-									 	}
-									}
-								}			
-							}
-							for(var l=0; l<holder.genres.length; l++){
-								if(holder.genres[l]){
-									var link = holder.genres[l];
-									var substrings = [genre];
-									var length = substrings.length;
-									 while(length--){
-										if(link.indexOf(substrings[length])!=-1){
-											$('.linkTable').append(`<tr><td><img src='${holder.images[l]}' width=175 height=175></img></td><td><a href='${holder.links[l]}'>${holder.titles[l]}</a></td></tr>`)							 	
-									 	}
-									}
-								}			
-							}
+							toFilteredList('blurbs')
+							toFilteredList('genres')
+							toFilteredList('titles')
 							console.log("filter finished")
 					})
 				})
@@ -130,6 +88,36 @@
 
 	});
 
+function toHolder(entries){
+	for(var i=0;i<entries['links'].length;i++){
+	 	holder['links'].push(entries.links[i]);
+	 	holder['images'].push(entries.images[i]);
+	 	holder['titles'].push(entries.titles[i]);
+	 	holder['blurbs'].push(entries.blurbs[i]);
+	}
+}
+
+function toList(entries){
+	for(var i=0; i<entries.links.length; i++){
+		$('.linkTable').append(`<tr><td><img src='${entries.images[i]}' width=230 height=230></img></td><td><a href='${entries.links[i]}'>${entries.titles[i]}</a></td></tr>`)
+	}
+}
+
+function toFilteredList(key){
+	for(var i=0; i < holder[key].length; i++){
+		if(holder[key][i]){
+			var link = holder[key][i];
+			var substrings = [genre];
+			var length = substrings.length;
+			 while(length--){
+				if(link.indexOf(substrings[length])!=-1){
+					$('.linkTable').append(`<tr><td><img src='${holder.images[i]}' width=200 height=200></img></td><td><a href='${holder.links[i]}'>${holder.titles[i]}</a></td></tr>`)		
+					console.log("found")					 	
+			 	}
+			}
+		}			
+	}
+}
  
 
 
